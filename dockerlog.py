03fbf695e01af851eca1e4693f57e732735db855
janlampe <janlampe25@gmail.com>
2019-09-16 15:37:49 +0200

    Add files via upload

---
+++ dockerlog.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+
+# logging_example.py
+
+import logging
+
+# Create a custom logger
+logger = logging.getLogger(__name__)
+
+# Create handlers
+c_handler = logging.StreamHandler()
+f_handler = logging.FileHandler('file.log')
+c_handler.setLevel(logging.INFO)
+f_handler.setLevel(logging.DEBUG)
+
+# Create formatters and add it to handlers
+c_format = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
+f_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+c_handler.setFormatter(c_format)
+f_handler.setFormatter(f_format)
+
+# Add handlers to the logger
+logger.addHandler(c_handler)
+logger.addHandler(f_handler)
+
+logger.info('This is an info ')
+logger.debug('This is a debug')
+
+print("this script loaded successfully")
+print("docker seems to have worked (for now)")
+
+
